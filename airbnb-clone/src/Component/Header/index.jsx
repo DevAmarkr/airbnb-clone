5812eab1abd8685f0af8bc5367005f0906f08c22
DevAmar <amar.dev2214@gmail.com>
2021-02-28 20:45:36 +0530

    Header Component created

---
+++ airbnb-clone/src/Component/Header/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import './index.css';
+import SearchIcon from '@material-ui/icons/Search';
+import LanguageIcon from '@material-ui/icons/Language';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+
+const Header = (props) => {
+  return (
+    <>
+      <header className="airbnb__header">
+          <img className ="airbnb__logo" src={process.env.PUBLIC_URL + "Image-assets/logo192.png"} />
+        <div className="airbnb__search">
+            <input type="text"/>
+            <SearchIcon/>
+        </div>
+        <div className="airbnb__profile">
+            <p>Become Host</p>
+            <LanguageIcon/>
+            <div className ="airbnb__profile--menu">
+                <MenuIcon className ="color-grey"/>
+                <AccountCircleIcon className ="color-grey"/>
+            </div>
+
+
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
---
+++ airbnb-clone/src/Component/Home/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './index.css';
+
+
+
+const Home = (props) => {
+  return (
+    <>
+      <div className ="home">
+          <p>Home</p>
+      </div>
+    </>
+  );
+};
+
+export default Home;
